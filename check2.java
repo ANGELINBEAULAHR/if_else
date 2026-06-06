14fa36b8e717423956f341eed48b6d14790ea101
ANGELIN BEAULAH R <angelinbeaulahr71@gmail.com>
2026-06-06 14:00:18 +0530

    Update check2.java

--- check2.java
+++ check2.java
@@ -1,3 +1,18 @@
 public class check2 {
-    
+    public static void main(Strings[] args)
+    {
+      check2 c = new check2();
+      System.out.println(c.com());
+      c.comp(2,3);      
+    }
+
+    public int comp(int a, int b){
+
+        System.out.println(a+b);
+
+    }
+    int d = 4;
+    public return com(){
+        return d;
+    }
 }
