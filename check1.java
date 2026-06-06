02db06d3473496909a70a1a18ac18e9a869493ef
ANGELINBEAULAHR <angelinbeaulahr71@gmail.com>
2026-06-06 13:48:14 +0530

    new file

---
+++ check1.java
@@ -0,0 +1,14 @@
+class check1{
+    int num1 = 36;
+    int num2 = 89;
+    public static void main(String[] args){
+    check1 c = new check1();
+    if(c.num1==c.num2){
+        System.out.println("same");
+    }
+    else{
+        System.out.println("not same");
+    }
+
+    }
+}
