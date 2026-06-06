99e4b6cf8a8473e6cc8c05bb9abf153951ddc046
ANGELINBEAULAHR <angelinbeaulahr71@gmail.com>
2026-06-06 14:26:44 +0530

    updated

--- check3.java
+++ check3.java
@@ -1,3 +1,25 @@
 public class check3 {
-    
+    public static void main(String[] args){
+        check3 c = new check3();
+        System.out.println(c.examp());
+         System.out.println(c.ex(56,78));
+          System.out.println(c.check(20));
+           System.out.println(c.name());
+}
+ public int examp(){
+    return 5;
+ }
+
+ public int ex(int a , int b){
+    return a+b;
+ }
+
+ public boolean check( int age){
+     return  age >=18;
+ }
+ String b = "bonjour";
+ public String name(){
+       System.out.println(b);
+       return   "hello";
+}
 }
